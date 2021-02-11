b875cce71e96782e14e54ee5ae3b1063cb43944c
Shruti Balasa <thirusofficial@gmail.com>
2021-02-11 22:55:12 +0530

    Timeline complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      inset: {
+        '17': '68px',
+        '26': '104px'
+      }
+    },
   },
   variants: {
     extend: {},
